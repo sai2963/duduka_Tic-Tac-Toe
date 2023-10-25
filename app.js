b5a0e407a45578f4307a0fb498cb8601a649caf3
DUDUKA VENKAT SAI <136826767+sai2963@users.noreply.github.com>
2023-10-25 06:00:07 -0700

    Update app.js

--- app.js
+++ app.js
@@ -12,116 +12,145 @@ document.addEventListener("DOMContentLoaded", function () {
     let backdropEl = document.getElementById("backdrop");
     let playernameEl = document.getElementById("playername");
     let gameboardEl = document.getElementById("game-board");
-    let gameElements = document.querySelectorAll('#game-board li');
-
+    let gameElements = document.querySelectorAll("#game-board li");
+  
     let activePlayer = ""; // Store the active player for the form submission
     let p1 = ""; // Declare p1 outside the event listener functions
     let p2 = ""; // Declare p2 outside the event listener functions
-
+  
     let currentPlayerIndex = 0;
-
+  
     const players = [
-        { name: "", symbol: 'X' },
-        { name: "", symbol: 'O' }
+      { name: "", symbol: "X" },
+      { name: "", symbol: "O" },
     ];
-
+  
     function openModal() {
-        backdropEl.style.display = "block";
-        modalEl.style.display = "block";
-        playernameEl.value = ""; // Clear the input value when the modal is opened
-        activePlayer = this.id === "btn3" ? "player1" : "player2";
+      backdropEl.style.display = "block";
+      modalEl.style.display = "block";
+      playernameEl.value = ""; // Clear the input value when the modal is opened
+      activePlayer = this.id === "btn3" ? "player1" : "player2";
     }
-
+  
     btn3El.addEventListener("click", openModal);
     btn4El.addEventListener("click", openModal);
-
+  
     function close() {
-        modalEl.style.display = "none";
-        backdropEl.style.display = "none";
+      modalEl.style.display = "none";
+      backdropEl.style.display = "none";
     }
-
+  
     btn1El.addEventListener("click", close);
-
+  
     // Event listener for the "Confirm" button
     document.getElementById("btn2").addEventListener("click", function (e) {
-        e.preventDefault(); // Prevent the default form submission
-        if (playernameEl.value.trim() === "") {
-            alert("Enter Your Name");
-            return; // Return early if the input is empty
-        }
-        let playername = playernameEl.value;
-
-        if (activePlayer === "player1") {
-            p1 = playername;
-            players[0].name = p1; // Update the player name in the players array
-            name1El.textContent = playername;
-        } else if (activePlayer === "player2") {
-            p2 = playername;
-            players[1].name = p2; // Update the player name in the players array
-            name2El.textContent = playername;
-        }
-
-        modalEl.style.display = "none";
-        backdropEl.style.display = "none";
-        playernameEl.value = ""; // Clear the input value after confirming the name
+      e.preventDefault(); // Prevent the default form submission
+      if (playernameEl.value.trim() === "") {
+        alert("Enter Your Name");
+        return; // Return early if the input is empty
+      }
+      let playername = playernameEl.value;
+  
+      if (activePlayer === "player1") {
+        p1 = playername.toUpperCase();
+        players[0].name = p1; // Update the player name in the players array
+        name1El.textContent = playername;
+      } else if (activePlayer === "player2") {
+        p2 = playername.toUpperCase();
+        players[1].name = p2; // Update the player name in the players array
+        name2El.textContent = playername;
+      }
+  
+      modalEl.style.display = "none";
+      backdropEl.style.display = "none";
+      playernameEl.value = ""; // Clear the input value after confirming the name
     });
-
+  
     function start() {
-        if (p1 === "" || p2 === "") {
-            alert("Enter Player Names");
-            return; // Return early if the input is empty
-        }
-        gameboardEl.style.display = "grid"; // Display the game board
-
-        // Scroll to the game board
-        gameboardEl.scrollIntoView({ behavior: 'smooth' });
-        playernamegEl.innerHTML = players[currentPlayerIndex].name; // Update player name in the game board
-
-        gameElements = document.querySelectorAll('#game-board li'); // Update the gameElements variable
-
-        for (let gameElement of gameElements) {
-            gameElement.addEventListener('click', selectGameField);
-        }
+      if (p1 === "" || p2 === "") {
+        alert("Enter Player Names");
+        return; // Return early if the input is empty
+      }
+      gameboardEl.style.display = "grid"; // Display the game board
+  
+      // Scroll to the game board
+      gameboardEl.scrollIntoView({ behavior: "smooth" });
+      playernamegEl.innerHTML = players[currentPlayerIndex].name; // Update player name in the game board
+  
+      gameElements = document.querySelectorAll("#game-board li"); // Update the gameElements variable
+  
+      for (let gameElement of gameElements) {
+        gameElement.addEventListener("click", selectGameField);
+      }
     }
-
+  
     newgameEl.addEventListener("click", start);
-
+  
     function checkWinCondition() {
-        const winCombos = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6]
-        ];
-
-        for (let combo of winCombos) {
-            if (
-                gameElements[combo[0]].textContent === players[currentPlayerIndex].symbol &&
-                gameElements[combo[1]].textContent === players[currentPlayerIndex].symbol &&
-                gameElements[combo[2]].textContent === players[currentPlayerIndex].symbol
-            ) {
-                return true;
-            }
+      const winCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+  
+      for (let combo of winCombos) {
+        if (
+          gameElements[combo[0]].textContent === players[currentPlayerIndex].symbol &&
+          gameElements[combo[1]].textContent === players[currentPlayerIndex].symbol &&
+          gameElements[combo[2]].textContent === players[currentPlayerIndex].symbol
+        ) {
+          return true;
         }
-        return false;
+      }
+      return false;
     }
-
+  
+    function checkDrawCondition() {
+      for (let gameElement of gameElements) {
+        if (gameElement.textContent === "") {
+          return false;
+        }
+      }
+      return true;
+    }
+  
+    function resetGame() {
+      resultEl.textContent = ""; // Clear the result text
+      for (let gameElement of gameElements) {
+        gameElement.textContent = ""; // Clear the game board
+        gameElement.addEventListener("click", selectGameField);
+        gameElement.classList.remove("disabled");
+      }
+    }
+  
+    newgameEl.addEventListener("click", resetGame);
+  
     function selectGameField(event) {
-        if (!event.target.textContent) {
-            event.target.textContent = players[currentPlayerIndex].symbol;
-            if (checkWinCondition()) {
-                resultEl.textContent = `${players[currentPlayerIndex].name} wins!`;
-                gameElements.forEach((element) => {
-                    element.removeEventListener('click', selectGameField);
-                });
-                return;
-            }
-            currentPlayerIndex = 1 - currentPlayerIndex; // Switch player
-            playernamegEl.innerHTML = players[currentPlayerIndex].name; // Update current player name
+      if (!event.target.textContent) {
+        event.target.textContent = players[currentPlayerIndex].symbol;
+        if (checkWinCondition()) {
+          resultEl.textContent = `${players[currentPlayerIndex].name} won the match`;
+          gameElements.forEach((element) => {
+            element.removeEventListener("click", selectGameField);
+            element.classList.add("disabled");
+          });
+          return;
         }
+  
+        if (checkDrawCondition()) {
+          resultEl.textContent = "Match Drawn";
+          return;
+        }
+  
+        currentPlayerIndex = 1 - currentPlayerIndex; // Switch player
+        playernamegEl.innerHTML = players[currentPlayerIndex].name; // Update current player name
+      }
+      event.target.classList.add("disabled");
     }
-});
+  });
+  
